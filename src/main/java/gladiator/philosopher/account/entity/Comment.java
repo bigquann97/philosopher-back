fb8e48476e062959fbfad3b7df73f445d40f48a5
h7359841 <h7359841@gmail.com>
2023-02-08 15:10:35 +0900

    feat: Comment Entity 생성(#11)
    Comment Entity 생성하였습니다.

---
+++ src/main/java/gladiator/philosopher/account/entity/Comment.java
@@ -0,0 +1,34 @@
+package gladiator.philosopher.account.entity;
+
+import gladiator.philosopher.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Comment extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(nullable = false)
+  private String commentContent;
+
+  /*
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "thread_id")
+  private Thread thread;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "account_id")
+  private Account account;
+
+  public Comment(Thread thread, Account account, String requestCommentContent) {
+    this.thread = thread;
+    this.account = account;
+    this.commentContent = requestCommentContent;
+  }
+   */
+
+}
