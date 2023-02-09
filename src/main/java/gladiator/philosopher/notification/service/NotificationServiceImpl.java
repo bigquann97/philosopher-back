c21bc0b2d949bfc335dd08a35a29fe34175ea073
Gwanho KIM <bigquan977@gmail.com>
2023-02-09 20:43:17 +0900

    feat: Notification 기본 로직 작성(#33)

--- src/main/java/gladiator/philosopher/notification/service/NotificationServiceImpl.java
+++ src/main/java/gladiator/philosopher/notification/service/NotificationServiceImpl.java
@@ -1,13 +1,44 @@
 package gladiator.philosopher.notification.service;
 
+import gladiator.philosopher.account.entity.Account;
+import gladiator.philosopher.notification.entity.Notification;
 import gladiator.philosopher.notification.repository.NotificationRepository;
+import gladiator.philosopher.post.entity.Post;
+import gladiator.philosopher.recommend.entity.Recommend;
+import gladiator.philosopher.recommend.service.RecommendService;
+import gladiator.philosopher.report.dto.NotificationResponseDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
   private final NotificationRepository notificationRepository;
+  private final RecommendService recommendService;
+
+  @Override
+  @Transactional
+  public void notifyToRecommendersThatThreadHasStarted(final Post post) {
+    List<Recommend> recommends = recommendService.getPostRecommends(post);
+    for (Recommend recommend : recommends) {
+      Account user = recommend.getAccount();
+      String content =
+          user.getNickname() + "님이 추천을 누른 \"" + post.getTitle() + "\" 게시글에 대한 회의장이 열렸습니다!";
+      Notification notification = Notification.builder().account(user).content(content).build();
+      notificationRepository.save(notification);
+    }
+  }
+
+  @Override
+  public List<NotificationResponseDto> getMyNotifications(Account member) {
+    List<Notification> notifications = notificationRepository.findByAccount(member);
+    return notifications.stream().map(NotificationResponseDto::new)
+        .collect(Collectors.toList());
+  }
+
 
 }
