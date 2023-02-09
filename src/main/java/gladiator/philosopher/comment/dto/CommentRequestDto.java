5d20cbfd02c6254f967d4f3a8ca0636b1a40cfbe
h7359841 <h7359841@gmail.com>
2023-02-09 15:07:46 +0900

    feat: Comment 설계 및 CRUD 진행(#11)
    Comment 관련 설계 및 CRUD를 진행하였습니다.

---
+++ src/main/java/gladiator/philosopher/comment/dto/CommentRequestDto.java
@@ -0,0 +1,15 @@
+package gladiator.philosopher.comment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CommentRequestDto {
+
+  private String content;
+
+  public CommentRequestDto(String content) {
+    this.content = content;
+  }
+}
---
+++ src/main/java/gladiator/philosopher/comment/dto/CommentResponseDto.java
@@ -0,0 +1,30 @@
+package gladiator.philosopher.comment.dto;
+
+import gladiator.philosopher.comment.entity.Comment;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CommentResponseDto {
+
+  private Long commentId;
+
+  private String nickname;
+
+  private String content;
+
+  private LocalDateTime createDate;
+
+  private LocalDateTime modifiedDate;
+
+  public CommentResponseDto(Comment comment) {
+    this.commentId = comment.getId();
+    this.nickname = comment.getAccount().getNickname();
+    this.content = comment.getContent();
+    this.createDate = comment.getCreateDate();
+    this.modifiedDate = comment.getModDate();
+  }
+
+}
