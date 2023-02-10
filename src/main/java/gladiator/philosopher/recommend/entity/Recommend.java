7c8bb0c2b4886ce47d24c5205e6ce10e2d9cb34c
dohwa <dohwa92@gmail.com>
2023-02-10 11:38:03 +0900

    feat: Recommend 관련 세부 로직 추가(#44)
    Recommend 관련 세부 로직 추가 구현

--- src/main/java/gladiator/philosopher/recommend/entity/Recommend.java
+++ src/main/java/gladiator/philosopher/recommend/entity/Recommend.java
@@ -2,7 +2,9 @@ package gladiator.philosopher.recommend.entity;
 
 
 import gladiator.philosopher.account.entity.Account;
+import gladiator.philosopher.comment.entity.Comment;
 import gladiator.philosopher.post.entity.Post;
+import gladiator.philosopher.thread.entity.Thread;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -32,31 +34,26 @@ public class Recommend {
   @JoinColumn(name = "post_id", nullable = false)
   private Post post;
 
-/*
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "thread_id")
   private Thread thread;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "comment_id")
-  private Comment commnet;
-*/
+  private Comment comment;
 
   public Recommend(Post post, Account account) {
     this.post = post;
     this.account = account;
   }
 
-/*
   public Recommend(Thread thread, Account account) {
     this.thread = thread;
     this.account = account;
   }
 
-  public Recommend(Comment commnet, Account account) {
-    this.commnet = commnet;
+  public Recommend(Comment comment, Account account) {
+    this.comment = comment;
     this.account = account;
   }
-*/
-
 }
--- src/main/java/gladiator/philosopher/recommend/service/RecommendServiceImpl.java
+++ src/main/java/gladiator/philosopher/recommend/service/RecommendServiceImpl.java
@@ -1,12 +1,16 @@
 package gladiator.philosopher.recommend.service;
 
 import gladiator.philosopher.account.entity.Account;
+import gladiator.philosopher.comment.entity.Comment;
+import gladiator.philosopher.comment.service.CommentService;
 import gladiator.philosopher.common.exception.CustomException;
 import gladiator.philosopher.common.exception.ExceptionStatus;
 import gladiator.philosopher.post.entity.Post;
-import gladiator.philosopher.post.repository.PostRepository;
+import gladiator.philosopher.post.service.PostService;
 import gladiator.philosopher.recommend.entity.Recommend;
 import gladiator.philosopher.recommend.repository.RecommendRepository;
+import gladiator.philosopher.thread.entity.Thread;
+import gladiator.philosopher.thread.repository.ThreadRepository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -18,35 +22,83 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecommendServiceImpl implements RecommendService {
 
   private final RecommendRepository recommendRepository;
-  private final PostRepository postRepository;
-
+  private final PostService postService;   // Impl말고 Service를 참조해야하게 수정필요
+  private final CommentService commentService;
+  //  private final ThreadService threadService;
+  private final ThreadRepository threadRepository;
 
   @Transactional
-  public void modifyRecommendPost(Long postId, Account account) {
-    Post post = postRepository.findById(postId)
-        .orElseThrow(() -> new CustomException(ExceptionStatus.POST_IS_NOT_EXIST));
+  public void createRecommendPost(Long postId, Account account) {
+    Post post = postService.getPostEntity(postId);
+    Optional<Recommend> recommendPost = recommendRepository.findByPostIdAndAccount(postId, account);
+    if (recommendPost.isPresent()) {
+      throw new IllegalArgumentException("이미 좋아요를 누르셨습니다.");
+    }
+    Recommend recommend = new Recommend(post, account);
+    recommendRepository.save(recommend);
+  }
+
+  @Transactional
+  public void deleteRecommendPost(Long postId, Account account) {
     Optional<Recommend> recommendPost = recommendRepository.findByPostIdAndAccount(postId, account);
     if (recommendPost.isEmpty()) {
-      Recommend recommend = new Recommend(post, account);
-      recommendRepository.save(recommend);
-    } else {
-      recommendRepository.delete(recommendPost.get());
+      throw new IllegalArgumentException("이미 좋아요 취소를 하셨습니다.");
     }
+    recommendRepository.delete(recommendPost.get());
   }
-/*
+
   @Transactional
-  public void modifyRecommendComment(Long commentId, Account account) {
-    Comment comment = commentRepository.findById(commentId)
-        .orElseThrow(() -> new CustomException(ExceptionStatus.POST_IS_NOT_EXIST));
-    Optional<Recommend> recommendComment = recommendRepository.findByCommentIdAndAccount(commentId, account);
-    if (recommendComment.isEmpty()) {
-      Recommend recommend = new Recommend(comment, account);
-      recommendRepository.save(recommend);
-    } else {
-      recommendRepository.delete(recommendComment.get());
+  public void createRecommendThread(Long threadId, Account account) {
+//    Thread thread = threadService.getThreadEntity(threadId);
+    Thread thread = threadRepository.findById(threadId).orElseThrow(
+        () -> new CustomException(ExceptionStatus.POST_IS_NOT_EXIST));
+    Optional<Recommend> recommendThread = recommendRepository.findByThreadIdAndAccount(threadId,
+        account);
+    if (recommendThread.isPresent()) {
+      throw new IllegalArgumentException("이미 좋아요를 누르셨습니다.");
     }
+    Recommend recommend = new Recommend(thread, account);
+    recommendRepository.save(recommend);
   }
-  */
+
+  @Transactional
+  public void deleteRecommendThread(Long threadId, Account account) {
+    Optional<Recommend> recommendThread = recommendRepository.findByThreadIdAndAccount(threadId,
+        account);
+    if (recommendThread.isEmpty()) {
+      throw new IllegalArgumentException("이미 좋아요 취소를 하셨습니다.");
+    }
+    recommendRepository.delete(recommendThread.get());
+  }
+
+  @Transactional
+  public void createRecommendComment(Long commentId, Account account) {
+    Comment comment = commentService.getCommentEntity(commentId);
+    Optional<Recommend> recommendComment = recommendRepository.findByCommentIdAndAccount(commentId,
+        account);
+    if (recommendComment.isPresent()) {
+      throw new IllegalArgumentException("이미 좋아요를 누르셨습니다.");
+    }
+    Recommend recommend = new Recommend(comment, account);
+    recommendRepository.save(recommend);
+  }
+
+  @Transactional
+  public void deleteRecommendComment(Long commentId, Account account) {
+    Optional<Recommend> recommendComment = recommendRepository.findByCommentIdAndAccount(commentId,
+        account);
+    if (recommendComment.isEmpty()) {
+      throw new IllegalArgumentException("이미 좋아요 취소를 하셨습니다.");
+    }
+    recommendRepository.delete(recommendComment.get());
+  }
+
+/*
+  public Long countRecommendPost(Long postId) {
+    Long countRecommendPost = recommendRepository.countByPostId(postId);
+    return countRecommendPost;
+  }
+*/
 
   @Override
   public List<Recommend> getPostRecommends(Post post) {
