2fb63bc203f62cc2d5ecc0cdaeca8199f0b74514
Gwanho KIM <bigquan977@gmail.com>
2023-02-10 14:09:29 +0900

    feat: Mention 세부 로직 구현(#39)

--- src/main/java/gladiator/philosopher/mention/repository/MentionRepository.java
+++ src/main/java/gladiator/philosopher/mention/repository/MentionRepository.java
@@ -1,8 +1,15 @@
 package gladiator.philosopher.mention.repository;
 
+import gladiator.philosopher.comment.entity.Comment;
 import gladiator.philosopher.mention.entity.Mention;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 public interface MentionRepository extends JpaRepository<Mention, Long> {
+
+  List<Mention> findByMentionedComment(@NonNull Comment mentionedComment);
+
+  List<Mention> findByMentioningComment(@NonNull Comment mentioningComment);
 
 }
--- src/main/java/gladiator/philosopher/mention/service/MentionServiceImpl.java
+++ src/main/java/gladiator/philosopher/mention/service/MentionServiceImpl.java
@@ -1,13 +1,73 @@
 package gladiator.philosopher.mention.service;
 
+import gladiator.philosopher.comment.entity.Comment;
+import gladiator.philosopher.comment.service.CommentService;
+import gladiator.philosopher.mention.entity.Mention;
 import gladiator.philosopher.mention.repository.MentionRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MentionServiceImpl implements MentionService {
 
   private final MentionRepository mentionRepository;
+  private final CommentService commentService;
+
+  // 댓글 내용 바탕으로 멘션 객체 생성
+  @Override
+  @Transactional
+  public void mentionComment(Comment mentioningComment) {
+    String content = mentioningComment.getContent().replaceAll(" ", ""); // 공백 제거
+    Long mentionedCommentId = parseFirstHashtagNum(content);
+
+    while (mentionedCommentId != null) {
+      Comment mentionedComment = commentService.getCommentEntity(mentionedCommentId);
+      Mention mention = Mention.builder()
+          .mentionedComment(mentionedComment)
+          .mentioningComment(mentioningComment)
+          .build();
+      mentionRepository.save(mention);
+      content = content.replaceAll("#" + mentionedCommentId, "");
+      mentionedCommentId = parseFirstHashtagNum(content);
+    }
+  }
+
+  // 파라미터 댓글이 MentioningComment 인 모든 Mention 가져오기
+  // 해당 댓글이 멘션중인 모든 댓글 가져오기
+  @Override
+  @Transactional
+  public List<Mention> getMentioningList(Comment mentioningComment) {
+    return mentionRepository.findByMentioningComment(mentioningComment);
+  }
+
+  // 파라미터 댓글이 MentionedComment 인 모든 Mention 가져오기
+  // 해당 댓글을 멘션한 모든 댓글 가져오기
+  @Override
+  @Transactional
+  public List<Mention> getMentionedList(Comment mentionedComment) {
+    return mentionRepository.findByMentionedComment(mentionedComment);
+  }
+
+  private Long parseFirstHashtagNum(String str) {
+    int firstSharpIdx = str.indexOf("#");
+    int pointer = firstSharpIdx + 1;
+
+    while (str.charAt(pointer) >= 48 && str.charAt(pointer) <= 57) {
+      pointer++;
+    }
+
+    String num = str.substring(firstSharpIdx + 1, pointer);
+
+    try {
+      return Long.parseLong(num);
+    } catch (NumberFormatException e) {
+      return null;
+    }
+  }
 
 }
