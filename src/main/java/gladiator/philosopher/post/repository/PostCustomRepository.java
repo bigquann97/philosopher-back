3d6df4c56f66c3593ee8e0596bacfbec649597d9
jipang9 <kiang18@naver.com>
2023-02-14 16:46:49 +0900

    feat : post부분 querydsl 작업 시작 (#90)

---
+++ src/main/java/gladiator/philosopher/post/repository/PostCustomRepository.java
@@ -0,0 +1,5 @@
+package gladiator.philosopher.post.repository;
+
+public interface PostCustomRepository {
+
+}
--- src/main/java/gladiator/philosopher/post/repository/PostImageRepository.java
+++ src/main/java/gladiator/philosopher/post/repository/PostImageRepository.java
@@ -3,6 +3,6 @@ package gladiator.philosopher.post.repository;
 import gladiator.philosopher.post.entity.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+public interface PostImageRepository extends JpaRepository<PostImage, Long>,PostCustomRepository {
 
 }
